3c701df3e0294bb73c3e8e37e60c71217f9de8a6
Varsha413 <varsha.srb.s@gmail.com>
2026-05-21 20:39:45 +0530

    Rename package and add main method for addition

--- src/main/java/com/example/CICD20231CDV0061/Calculator.java
+++ src/main/java/com/example/CICD20231CDV0061/Calculator.java
@@ -1,5 +1,14 @@
-package com.example.CICD20231CDV0061;
+package com.example.lab1practice;
 
 public class Calculator {
 
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("Addition = " + sum);
+    }
 }
